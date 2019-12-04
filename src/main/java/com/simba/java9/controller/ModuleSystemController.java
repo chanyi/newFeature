8321b631debe52161ff431f0ad2fb8708acd9d63
lilei <lilei@ut.cn>
2019-12-04 09:16:48 +0800

    feat(java9):增加模块系统

---
+++ src/main/java/com/simba/java9/controller/ModuleSystemController.java
@@ -0,0 +1,13 @@
+package com.simba.java9.controller;
+
+/**
+ * java9新特性：模块系统
+ *    java9的最大变化
+ *    模块是一个包的容器
+ *    java保持向后兼容，所以jdk越来越大，即使是一个helloword也需要加载几乎所有的api
+ *    Jigsaw 致力于把java平台和jdk分解成更小更有组织的模块，使用时只需要加载需要的模块即可，无需加载所有的api
+ *    使用module-info.java解释包信息
+ */
+public class ModuleSystemController {
+
+}
